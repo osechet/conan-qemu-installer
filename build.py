6f27e4e555522e27d0b915ff6c424eff5c7ef1cc
Olivier Sechet <osechet@gmail.com>
2019-10-16 12:32:07 +0200

    Fix build.py

--- build.py
+++ build.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 import os
 import platform
 from conans.tools import OSInfo
 from cpt.packager import ConanMultiPackager
 
 AVAILABLE_VERSIONS = [
-    "4.1.0", 
-    "4.0.0", 
-    "3.1.1.1"
+    "4.1.0",
+    "4.0.0",
+    "3.1.1.1",
 ]
 
 def detected_os():
@@ -20,13 +19,13 @@ def detected_os():
 def main():
     arch = os.environ["CONAN_ARCHS"]
     builder = ConanMultiPackager()
-    os = detected_os()
+    current_os = detected_os()
 
     for version in AVAILABLE_VERSIONS:
         # New mode, with version field
-        builder.add({"os" : os, "arch_build" : arch, "arch": arch}, {}, {}, {}, reference="cmake_installer/%s" % version)
+        builder.add({"os" : current_os, "arch_build" : arch, "arch": arch}, {}, {}, {}, reference="cmake_installer/%s" % version)
 
     builder.run()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
